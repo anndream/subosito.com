4877a1351cdb05038b68f03e2b8397fa6fde6906
Alif Rachmawadi <arch@subosito.com>
2020-03-15 21:49:27 +0700

    add homepage

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function() {
+  return (
+    <>
+      <Head>
+        <link href="/favicon.png" rel="shortcut icon" />
+      </Head>
+      <h1>@subosito</h1>
+    </>
+  )
+}
+
